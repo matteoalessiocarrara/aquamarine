80c96472f78ea89a9e38fefbc8367432b5b46f40
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2017-03-05 10:25:09 +0100

    Optimization

--- kernel/vga_console.c
+++ kernel/vga_console.c
@@ -14,8 +14,8 @@ void vga_init() {
 }
 
 void vga_scroll() {
-	for(register uint16_t i = 0; i < (VGA_CHARS - VGA_WIDTH); i++)
-		VGA_BUFF[i] = VGA_BUFF[i + VGA_WIDTH];
+	struct s {uint16_t c[VGA_CHARS - VGA_WIDTH];};
+	*(struct s*)VGA_BUFF = *(struct s*)(VGA_BUFF + VGA_WIDTH);
 	for(register uint8_t i = 0; i < VGA_WIDTH; i++)
 		VGA_BUFF[VGA_CHARS - VGA_WIDTH + i] = vga_make_char(' ', vga_color);
 	vga_cursor = VGA_CHARS - VGA_WIDTH;
